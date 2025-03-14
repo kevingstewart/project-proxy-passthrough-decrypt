26877c25465b1c4e4d63f6e4b65897e309e6c85c
Kevin Stewart <kevin.g.stewart@gmail.com>
2025-03-14 10:31:19 -0400

    Update proxy-passthrough-decrypt-installer.sh

--- proxy-passthrough-decrypt-installer.sh
+++ proxy-passthrough-decrypt-installer.sh
@@ -19,7 +19,13 @@ echo "..Creating a stub CA key"
 
 ## Create the iRule
 echo "..Creating the iRule"
-rule=$(curl -sk $(${baseurl}/refs/heads/main/
+rule=$(curl -sk $(${baseurl}/refs/heads/main/proxy-passthrough-rule | awk '{printf "%s\\n", $0}' | sed -e 's/\"/\\"/g;s/\x27/\\'"'"'/g')
+data="{\"name\":\"proxy-passthrough-rule\",\"apiAnonymous\":\"${rule}\"}"
+curl -sk \
+-u ${BIGUSER} \
+-H "Content-Type: application/json" \
+-d "${data}" \
+https://localhost/mgmt/tm/ltm/rule -o /dev/null
 
 ## Create the client SSL profile
 echo "..Creating the SSLFWD client SSL profile"
